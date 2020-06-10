0b0e985640ecfef24b5c319fbc12d7ed2b6bd418
YadelskyiDev <leCarreacute@gmail.com>
2020-06-10 15:07:17 +0300

    Rate

--- src/Rate/Rate.js
+++ src/Rate/Rate.js
@@ -2,27 +2,49 @@ import React from 'react';
 import './Rate.css';
 
 export class Rate extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      date : '',
+      currencyRate : {},
+    }
+    this.currency = ['USD', 'RUB', 'CAD', 'PHP'];
+  }
+
+  getRate = ()=>{
+    fetch('https://api.exchangeratesapi.io/latest')
+      .then(data =>{
+        return data.json();
+      })
+      .then(data =>{
+        this.setState({ date : data.date});
+        let result = {};
+        for(let i = 0; i < this.currency.length; i++){
+          result[this.currency[i]] = data.rates[this.currency[i]];
+        }
+        this.setState({currencyRate: result});
+      })
+  }
+
+  componentDidMount() {
+    this.getRate();
+  }
+  
   render(){
+    const { date, currencyRate } = this.state;
     return (
       <div className="rate">
-        <h3> Курс валют на </h3>
+        <h3> Курс валют на {date}</h3>
         <div className="flex-container">
-          <div className="block flex-item">
-            <div className="currency-name">USD</div>
-            <div className="currency-in">1500 Kr</div>
-            <div className="currency-out">1200 Kr</div>
-          </div>
-          <div className="block flex-item">
-            <div className="currency-name">USD</div>
-            <div className="currency-in">1500 Kr</div>
-            <div className="currency-out">1200 Kr</div>
-          </div>
-          <div className="block flex-item">
-            <div className="currency-name">USD</div>
-            <div className="currency-in">1500 Kr</div>
-            <div className="currency-out">1200 Kr</div>
-          </div>
-        </div>
+          {Object.keys(currencyRate).map((keyName, i)=> (
+            <div className="block flex-item" key={keyName}>
+              <div className="currency-name">{keyName}</div>
+              <div className="currency-in">{currencyRate[keyName].toFixed(2)}</div>
+              <p>*Можна купити за 1 EUR</p>
+            </div>
+          )
+          )}
+      </div>
         <h3> Калькулятор обміну</h3>
         <div className="block">
           <div>Я хочу</div>
